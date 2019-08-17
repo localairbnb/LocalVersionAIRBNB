25411e8ee3b12149021e9dc551921926e0e67405
Paul <pauldwamena10@gmail.com>
2019-08-17 21:09:46 +0000

    added cloudinary, multer functionalities including setting ptofile pic

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const port=8080;
 const listing=require('./controller/controller');
 const data=new listing();
 const userAuthentication=require('./controller/userAuthentication');
+const cloudinary=require('cloudinary');
+require ('./config/cloudinary');
+const upload=require('./config/multer');
+const helper=require('./controller/helper')
+const config=require('./config/clientConfig')
+const  db= config.mydb
+const auth=config.auth
+
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:false}));
@@ -17,9 +25,32 @@ app.get('/getAllListings', function(req, res) {
     
 })
 
+
+
+
 app.post('/register',userAuthentication.User.signup);
 app.post('/login',userAuthentication.User.login);
-// this endpoint registers a node and broadcast
+app.post('/setProfileImage',helper.checkifuserisloggedin,upload.single('image'), async(req,res)=>{
+    const result= await cloudinary.v2.uploader.upload(req.file.path)
+    var image=result.secure_url
+    let profilepic=image;
+    var id=req.user.uid
+    let userRef = db.collection('users').doc(id);
+    userRef.update({photourl: profilepic}).then(function(){
+        res.status(200).json("You have successfully update your profile picture")
+
+    }).catch(function(err){
+        res.status(400).json("An error occured and couldnt upload photo")
+    })
+    
+
+    
+    
+    })
+
+
+
+
 app.post('/addlisting', function(req, res) {
     var email,desc,phone,price;
     var images=[];
